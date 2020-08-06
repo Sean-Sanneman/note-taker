77128d8f63f419481a265c0a1194eef9483ea486
S. Sanneman <seandafydd@gmail.com>
2020-08-06 13:29:06 -0700

    everything works. I'm going to try adding a delete function now.

--- db/create.js
+++ db/create.js
@@ -1,17 +1,52 @@
+const fs = require("fs");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 class Create {
-    //create a function that reads a file
+  //create a function that reads a file
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+  //create function to write file
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
 
-//create function to write file
+  //create function to grab notes
+  getNote() {
+    return this.read().then((notes) => {
+      let notesArr;
+      try {
+        notesArr = [].concat(JSON.parse(notes));
+      } catch (err) {
+        notesArr = [];
+      }
+      return notesArr;
+    });
+  }
 
-//create function to grab notes
-
-//create function to add notes
-
+  //create function to add notes
+  addNote(note) {
+    console.log(note);
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      id: note.id,
+    };
+    return this.getNote()
+      .then((notes) => {
+        console.log(notes);
+        return [...notes, newNote];
+      })
+      .then((newNoteArr) => {
+        console.log(newNoteArr);
+        console.log(this);
+        this.write(newNoteArr);
+      })
+      .then(() => newNote);
+  }
 }
 
-
-
-
-
-
-module.exports = new Create
+module.exports = new Create();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,17 @@
 const path = require("path");
 const router = require("express").Router();
-const db = require("../db/db.json")
+const db = require("../db/db.json");
+const create = require("../db/create.js");
 
-router.get("/api/notes", (req, res) => {
-    return res.json(db);
+router.get("/notes", (req, res) => {
+ 
+ create.getNote().then(notes => res.json(notes))
+ .catch(err => console.log(err));
 });
 
-
-
-
-
-
-
-
-
-
-
-
+router.post("/notes", (req, res) => {
+    create.addNote(req.body)
+    .then((note) => res.json(note))
+    .catch(err => console.log(err));
+})
 module.exports = router;
