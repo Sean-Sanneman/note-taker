4ba6f89553e6bfb1c4777cd2e192dfab282e1b6b
S. Sanneman <seandafydd@gmail.com>
2020-08-08 17:31:15 -0700

    deployed on heroku, finished cleaning code

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-//require("./routes/apiRoutes.js")(app);
-//require("./routes/htmlRoutes.js")(app);
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-
-
-
+// link to app on heroku https://ssanneman-note-taker.herokuapp.com/
 
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
